e051a61cc846f7d3353f4a815a1924ef72c8da3c
Elisabeth Azémard <elie.azemard@gmail.com>
2019-10-31 13:50:36 +0100

    Add NavigationComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
+        <app-navigation></app-navigation>
         <router-outlet></router-outlet>
     `
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { RouterModule } from '@angular/router';
 // Inner
 import { AppComponent } from './app.component';
 import { MainRouter } from './app.router';
+import { NavigationComponent} from "./components/navigation/navigation.component";
 
 // Definition
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        NavigationComponent
     ],
     imports: [
         BrowserModule,
